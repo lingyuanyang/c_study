4237e82d866c770d8f640b0a9ec38bb42230a0ac
lingyuanyang <lingyuanyang@mininglamp.com>
2021-01-07 11:56:52 +0800

    test3

---
+++ DailyStudy/20201204.c
@@ -0,0 +1,37 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+// std standard input output
+
+/*
+%d -> int short long
+%c -> char
+%f -> float double(%lf)
+%p(pointer) -> <addr> &a
+%x -> 16
+%o
+%s -> array
+
+
+extern
+*/
+//int main() 
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	//& get_addr_char
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//
+//	//print function
+//	//char ch = 'A';
+//	/*printf("%c\n", ch);
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));*/
+//	//printf("Hello, C!\n");
+//	return 0;
+//}
---
+++ DailyStudy/20201214.c
@@ -0,0 +1,64 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include <stdio.h>
+//#include <string.h>
+
+//int main() {
+//	int sum;
+//	sum = Add(10, 20);
+//	printf("sum -> %d", sum);
+//	return 0;
+//}
+
+//int main() {
+//	int i = 0;
+//	while(i < 100) {
+//		printf("study\n");
+//		i++;
+//	}
+//	printf("Congratulation!, you have get the Tsinghua University MEM");
+//}
+//
+//int Add(int a, int b) {
+//	return a + b;
+//}
+
+/*
+shutdown -s -t 60
+shutdown -a
+system("shutdown -s -t 60")
+system("shutdown -a")
+strcmp("")  string compare
+*/
+
+//int main() {
+//    int input = 0;
+//    printf("Tsinghua University or Wuhan University?(0/1)");
+//    scanf("%d", &input);
+//    if (input == 0)
+//        printf("Beijing");
+//    else 
+//        printf("Wuhan");
+//    return 0;
+//}
+
+//int main() {
+//	char arr1[] = "abc";
+//	char arr2[] = {'a','b','c', '\0'};
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
+
+//int main(){
+//	// \0 -> string close tag
+//	// ASCII
+//	// a -> 97
+//	// A -> 65
+//	// \0-> 0
+//	char arr1[] = "abc";
+//	char arr2[] = {'a','b','c','\0'};
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
